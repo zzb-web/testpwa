fb0325d9dade0c05a7863c625ba58bf3cd40a806
zzb <1144181633@qq.com>
2023-05-19 16:49:38 +0800

    1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React PWA
+          Learn React 1
         </a>
       </header>
     </div>
