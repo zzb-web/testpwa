91843c37605379a7fb8339104af58c7f8585fea4
zzb <1144181633@qq.com>
2023-05-22 10:25:09 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -95,5 +95,5 @@
     self.addEventListener("install",event=>{})
 self.addEventListener("active",event=>{})
 self.addEventListener("fetch",event=>{
-//   event.responWith(fetch(event.request));
+  event.responWith(fetch(event.request));
 })
